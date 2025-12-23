a2bde6a685f71b788d7aeb8b4a64f1c15b2e9948
Hubert-Zochowski <zochowski92@gmail.com>
2025-12-23 14:33:06 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,143 @@
+/* PLIK DANYCH (data.js) */
+
+window.myAppConfig = {
+    profile: {
+        name: "Hubert Żochowski",
+        image: "", 
+        welcomeMessage: "Moja Kolekcja Kultury",
+        bio: "Oto lista książek i filmów, które kształtują moje myślenie.",
+        buttonText: "Przeglądaj listę",
+        
+        // --- NOWE DANE KONTAKTOWE ---
+        email: "zochowski92@gmail.com",
+        socialLinks: [
+            { name: "LinkedIn", url: "https://www.linkedin.com/in/hubert-zochowski/" }
+        ]
+    },
+
+    items: [
+        // --- NOWE POZYCJE (2025) ---
+        {
+            id: 10,
+            title: "Rozmyślania",
+            author: "Marek Aureliusz",
+            category: "book",
+            rating: 9,
+            date: "2025-06-01",
+            image: "", 
+            description: `Ponadczasowa mądrość stoicka. Cesarz rzymski pisze sam do siebie o tym, jak radzić sobie z trudnościami, ludźmi i własnym umysłem.`,
+            link: "https://lubimyczytac.pl/ksiazka/rozmyslania"
+        },
+        {
+            id: 11,
+            title: "Oczy Szeroko Otwarte",
+            author: "Marek Bartosiak",
+            category: "book",
+            rating: 8,
+            date: "2025-05-01",
+            image: "", 
+            description: `(Uzupełnij opis...)`,
+            link: "https://lubimyczytac.pl/ksiazka/5171089/oczy-szeroko-otwarte-strategia-polski-na-czas-wojny-swiatowej"
+        },
+        {
+            id: 12,
+            title: "Przeszkoda czy wyzwanie",
+            author: "Ryan Holiday", // Zakładam, że chodzi o tę książkę ("The Obstacle Is the Way")
+            category: "book",
+            rating: 8,
+            date: "2025-04-01",
+            image: "", 
+            description: `Stoicyzm w praktyce. O tym, jak to, co stoi nam na drodze, staje się drogą.`,
+            link: "https://lubimyczytac.pl/ksiazka/285251/przeszkoda-czy-wyzwanie-stoicka-sztuka-przekuwania-problemow-w-sukcesy"
+        },
+        {
+            id: 13,
+            title: "Dzień dobry potworku",
+            author: "Catherine Gildiner", 
+            category: "book", // Sprawdź czy to książka czy film
+            rating: 7,
+            date: "2025-03-01",
+            image: "", 
+            description: `(Uzupełnij opis...)`,
+            link: "https://lubimyczytac.pl/ksiazka/5023781/dzien-dobry-potworku-historie-z-terapii-ktore-inspiruja-do-walki-o-samego-siebie"
+        },
+        {
+            id: 14,
+            title: "Mężczyzna o pięknym głosie",
+            author: "Lillian B. Rubin", // Uzupełnij autora/reżysera
+            category: "book", // Strzelam, że to film/serial? Zmień na 'book' jeśli książka
+            rating: 7,
+            date: "2025-02-01",
+            image: "", 
+            description: `(Uzupełnij opis...)`,
+            link: "https://lubimyczytac.pl/ksiazka/5073937/mezczyzna-o-pieknym-glosie-opowiesci-o-terapii-ktora-lamie-reguly"
+        },
+        {
+            id: 15,
+            title: "Odwaga bycia nielubianym",
+            author: "Ichiro Kishimi, Fumitake Koga",
+            category: "book",
+            rating: 10,
+            date: "2025-01-01",
+            image: "", 
+            description: `Japoński fenomen. Dialog między filozofem a młodym człowiekiem oparty na psychologii Adlera. O tym, jak odzyskać wolność i przestać żyć oczekiwaniami innych.`,
+            link: "https://lubimyczytac.pl/ksiazka/odwaga-bycia-nielubianym"
+        },
+        {
+            id: 16,
+            title: "Wielka wojna o chipy",
+            author: "Chris Miller",
+            category: "book",
+            rating: 9,
+            date: "2025-08-01",
+            image: "", 
+            description: `Fascynująca historia technologii, która rządzi światem. Jak walka o półprzewodniki kształtuje geopolitykę USA i Chin.`,
+            link: "https://lubimyczytac.pl/ksiazka/5061451/wielka-wojna-o-chipy-jak-usa-i-chiny-walcza-o-technologiczna-dominacje-nad-swiatem"
+        },
+		        {
+            id: 17,
+            title: "Minuta ciszy",
+            author: "Jacek Lusiński",
+            category: "series",
+            rating: 9,
+            date: "2025-11-08",
+            image: "", 
+            description: `Opowieść o branży pogrzebowej i lokalnej społeczności`,
+            link: "https://www.filmweb.pl/serial/Minuta+ciszy-2022-10003607"
+        },
+		{
+            id: 18,
+            title: "Heweliusz",
+            author: "Jan Holoubek",
+            category: "series",
+            rating: 9,
+            date: "2025-12-01",
+            image: "", 
+            description: `Fascynująca historia technologii, która rządzi światem. Jak walka o półprzewodniki kształtuje geopolitykę USA i Chin.`,
+            link: "https://www.filmweb.pl/serial/Heweliusz-2025-10025579"
+        },
+    ],
+	// --- NOWA ODRĘBNA SEKCJA CYTATÓW ---
+    quotes: [
+        {
+            text: "Nasz los zależy od naszych myśli. Niech więc będą one dobre, proste i pełne życzliwości.",
+            author: "Marek Aureliusz"
+        },
+        {
+            text: "Wolność to bycie nielubianym przez innych ludzi. To dowód na to, że żyjesz własnym życiem.",
+            author: "Ichiro Kishimi"
+        },
+        {
+            text: "Nie wolno się bać, strach zabija duszę. Strach to mała śmierć, a wielkie unicestwienie.",
+            author: "Frank Herbert"
+        },
+        {
+            text: "Im więcej wiesz, tym mniej musisz mówić.",
+            author: "Jim Rohn"
+        },
+        {
+            text: "Zacznij tam gdzie jesteś, użyj tego co masz, zrób co możesz.",
+            author: "Arthur Ashe"
+        }
+    ]
+};
